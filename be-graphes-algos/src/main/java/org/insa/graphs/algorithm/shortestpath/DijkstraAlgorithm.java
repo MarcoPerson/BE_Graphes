dee974fdc26453ff09c8650c5d791db82b49a2dc
Agbeti-Messan Komlan Epe-Nsin Merveilles <agbeti-m@insa-20730>
2022-04-15 10:49:27 +0200

    Algo de Dikstra Terminé

--- be-graphes-algos/src/main/java/org/insa/graphs/algorithm/shortestpath/DijkstraAlgorithm.java
+++ be-graphes-algos/src/main/java/org/insa/graphs/algorithm/shortestpath/DijkstraAlgorithm.java
@@ -1,12 +1,16 @@
 package org.insa.graphs.algorithm.shortestpath;
 
-import java.util.Arrays;
-
+import java.util.ArrayList;
+import org.insa.graphs.algorithm.AbstractSolution.Status;
 import org.insa.graphs.algorithm.utils.BinaryHeap;
 import org.insa.graphs.model.Arc;
 import org.insa.graphs.model.Graph;
 import org.insa.graphs.model.Label;
 import org.insa.graphs.model.Node;
+import org.insa.graphs.model.Path;
+
+import java.util.Collections;
+
 
 public class DijkstraAlgorithm extends ShortestPathAlgorithm {
 
@@ -23,27 +27,69 @@ public class DijkstraAlgorithm extends ShortestPathAlgorithm {
         // Notify observers about the first event (origin processed).
         notifyOriginProcessed(data.getOrigin());
         
-        // Initialize array of successors.
-        Arc[] successorsArcs = new Arc[nbNodes];
+        // Initialize array.
+        Arc[] arcSuccessors = new Arc[nbNodes];
         
         //Initialize array of label 
         Label[] tabLabel = new Label[nbNodes];
         
         for(Node node : graph.getNodes()) {
-        	tabLabel[node.getId()] = new Label(node.getId(), false, 0);
+        	tabLabel[node.getId()] = new Label(node, false, null);
         } 
         
-        tabLabel[data.getOrigin().getId()] = new Label (data.getOrigin().getId(), true, data.getOrigin().getId());
+        tabLabel[data.getOrigin().getId()] = new Label (data.getOrigin(), true, data.getOrigin());
         tabLabel[data.getOrigin().getId()].setCost(0);
         
-        BinaryHeap heapLabel = new BinaryHeap();
+        BinaryHeap<Label> heapLabel = new BinaryHeap<Label>();
         for (Label label : tabLabel) {
-        	heapLabel.insert((Comparable) label);
+        	heapLabel.insert(label);
         }
         
+        while(!isAllMarkedTrue(tabLabel)) {
+        	Label labelMin = heapLabel.findMin();
+        	labelMin.setMarque(true);
+        	
+        	//The node has been marked, notify the observers
+        	notifyNodeReached(labelMin.getSommetCourant());
+        	
+        	heapLabel.remove(labelMin);
+        	for (Arc arc : labelMin.getSommetCourant().getSuccessors()) {
+        		Label labelCourant = tabLabel[arc.getDestination().getId()];
+        		
+        		if(labelCourant.getCost()>labelMin.getCost()+arc.getLength()) {
+        			heapLabel.remove(labelCourant);
+        			labelCourant.setCost(labelMin.getCost()+arc.getLength());
+        			labelCourant.setPere(labelMin.getSommetCourant());
+        			heapLabel.insert(labelCourant);
+        			arcSuccessors[labelCourant.getSommetCourant().getId()]= arc;
+        		}
+        	}
+        }
         
-        ShortestPathSolution solution = null;
-        // TODO:
+        ShortestPathSolution solution;
+		// Destination has no predecessors, the solution is infeasible...
+        if (arcSuccessors[data.getDestination().getId()] == null) {
+            solution = new ShortestPathSolution(data, Status.INFEASIBLE);
+        }
+        else {
+
+            // The destination has been found, notify the observers.
+            notifyDestinationReached(data.getDestination());
+
+            // Create the path from the array of successors...
+            ArrayList<Arc> arcs = new ArrayList<>();
+            Arc arc = arcSuccessors[data.getDestination().getId()];
+            while (arc != null) {
+                arcs.add(arc);
+                arc = arcSuccessors[arc.getOrigin().getId()];
+            }
+
+            // Reverse the path...
+            Collections.reverse(arcs);
+
+            // Create the final solution.
+            solution = new ShortestPathSolution(data, Status.OPTIMAL, new Path(graph, arcs));
+        }
         return solution;
     }
     
--- be-graphes-model/src/main/java/org/insa/graphs/model/Label.java
+++ be-graphes-model/src/main/java/org/insa/graphs/model/Label.java
@@ -1,16 +1,16 @@
 package org.insa.graphs.model;
 
-public class Label {
-	private int sommetCourant ;
+public class Label implements Comparable<Label>{
+	private Node sommetCourant ;
 	private boolean marque ;
 	private double cout;
-	private int pere;
+	private Node pere;
 	
-	public Label(int sommetCourant, boolean marque, int pere) {
-		this.sommetCourant = sommetCourant;
-		this.marque = marque;
+	public Label(Node sommetCourant, boolean marque, Node pere) {
+		this.setSommetCourant(sommetCourant);
+		this.setMarque(marque);
 		this.cout = Double.POSITIVE_INFINITY;
-		this.pere = pere;
+		this.setPere(pere);
 	}
 	
 	public double getCost() {
@@ -21,12 +21,40 @@ public class Label {
 		return this.marque;
 	}
 	
+	public void setMarque(boolean marque) {
+		this.marque = marque;
+	}
+	
 	public void setCost(double cost) {
 		this.cout = cost;
 	}
 	
-	public double compareTo(Label autre) {
-		return this.cout - autre.getCost();
+	public int compareTo(Label autre) {
+		int result;
+		if(this.cout == autre.getCost()){
+			result = 0;
+		} else if( this.cout >autre.getCost()){
+			result = 1;
+		}else{
+			result = -1;
+		}
+		return result;
+	}
+
+	public Node getSommetCourant() {
+		return sommetCourant;
+	}
+
+	public void setSommetCourant(Node sommetCourant) {
+		this.sommetCourant = sommetCourant;
+	}
+
+	public Node getPere() {
+		return pere;
+	}
+
+	public void setPere(Node pere) {
+		this.pere = pere;
 	}
 
 }
