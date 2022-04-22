38e4becabf60e047e842a9a6f62ccb55f991b87c
Agbeti-Messan Komlan Epe-Nsin Merveilles <agbeti-m@insa-20728>
2022-04-22 10:52:31 +0200

    Dijkstra fini, entrain de faire les verifications automatiques

--- be-graphes-algos/src/main/java/org/insa/graphs/algorithm/shortestpath/DijkstraAlgorithm.java
+++ be-graphes-algos/src/main/java/org/insa/graphs/algorithm/shortestpath/DijkstraAlgorithm.java
@@ -1,6 +1,9 @@
 package org.insa.graphs.algorithm.shortestpath;
 
 import java.util.ArrayList;
+
+import org.insa.graphs.algorithm.AbstractInputData;
+import org.insa.graphs.algorithm.AbstractInputData.Mode;
 import org.insa.graphs.algorithm.AbstractSolution.Status;
 import org.insa.graphs.algorithm.utils.BinaryHeap;
 import org.insa.graphs.model.Arc;
@@ -14,7 +17,8 @@ import java.util.Collections;
 
 public class DijkstraAlgorithm extends ShortestPathAlgorithm {
 
-    public DijkstraAlgorithm(ShortestPathData data) {
+
+	public DijkstraAlgorithm(ShortestPathData data) {
         super(data);
     }
 
@@ -23,6 +27,8 @@ public class DijkstraAlgorithm extends ShortestPathAlgorithm {
         final ShortestPathData data = getInputData();
         Graph graph = data.getGraph();
         final int nbNodes = graph.size();
+        
+        System.out.println("MODE :"+data.getMode());
         
         // Notify observers about the first event (origin processed).
         notifyOriginProcessed(data.getOrigin());
@@ -45,27 +51,73 @@ public class DijkstraAlgorithm extends ShortestPathAlgorithm {
         	heapLabel.insert(label);
         }
         
-        while(!isAllMarkedTrue(tabLabel)) {
-        	Label labelMin = heapLabel.findMin();
-        	labelMin.setMarque(true);
-        	
-        	//The node has been marked, notify the observers
-        	notifyNodeReached(labelMin.getSommetCourant());
-        	
-        	heapLabel.remove(labelMin);
-        	for (Arc arc : labelMin.getSommetCourant().getSuccessors()) {
-        		Label labelCourant = tabLabel[arc.getDestination().getId()];
-        		
-        		if(labelCourant.getCost()>labelMin.getCost()+arc.getLength()) {
-        			heapLabel.remove(labelCourant);
-        			labelCourant.setCost(labelMin.getCost()+arc.getLength());
-        			labelCourant.setPere(labelMin.getSommetCourant());
-        			heapLabel.insert(labelCourant);
-        			arcSuccessors[labelCourant.getSommetCourant().getId()]= arc;
-        		}
+        boolean destinationReached = false;
+		        
+        while(!isAllMarkedTrue(tabLabel) && !destinationReached) {
+        	//Dijkstra en distance
+        	if(data.getMode() == AbstractInputData.Mode.LENGTH) {
+        		Label labelMin = heapLabel.findMin();
+            	labelMin.setMarque(true);
+            	
+            	//The node has been marked, notify the observers
+            	notifyNodeMarked(labelMin.getSommetCourant());
+            	
+            	heapLabel.remove(labelMin);
+            	for (Arc arc : labelMin.getSommetCourant().getSuccessors()) {
+            		if(data.isAllowed(arc)) {
+            			Label labelCourant = tabLabel[arc.getDestination().getId()];
+                		
+                		if(labelCourant.getCost()>labelMin.getCost()+arc.getLength()) {
+                			
+                			//Pour effectuer le changement dans le tas, on remove, on modifie puis on insert
+                			heapLabel.remove(labelCourant);
+                			labelCourant.setCost(labelMin.getCost()+arc.getLength());
+                			labelCourant.setPere(labelMin.getSommetCourant());
+                			heapLabel.insert(labelCourant);
+                			arcSuccessors[labelCourant.getSommetCourant().getId()]= arc;
+                			
+                			//Verifie si la destination est atteinte - si oui on sort du for et du while
+                			if(data.getDestination()==labelCourant.getSommetCourant()) {
+                				destinationReached = true;
+                				break;
+                			}
+                		}
+            		}
+            	}
         	}
+        	//Dijkstra en temps
+        	else if(data.getMode() == AbstractInputData.Mode.TIME){
+        		Label labelMin = heapLabel.findMin();
+            	labelMin.setMarque(true);
+            	
+            	//The node has been marked, notify the observers
+            	notifyNodeMarked(labelMin.getSommetCourant());
+            	
+            	heapLabel.remove(labelMin);
+            	for (Arc arc : labelMin.getSommetCourant().getSuccessors()) {
+            		if(data.isAllowed(arc)) {
+            			Label labelCourant = tabLabel[arc.getDestination().getId()];
+                		
+                		if(labelCourant.getCost()>labelMin.getCost()+arc.getMinimumTravelTime()) {
+                			
+                			//Pour effectuer le changement dans le tas, on remove, on modifie puis on insert
+                			heapLabel.remove(labelCourant);
+                			labelCourant.setCost(labelMin.getCost()+arc.getMinimumTravelTime());
+                			labelCourant.setPere(labelMin.getSommetCourant());
+                			heapLabel.insert(labelCourant);
+                			arcSuccessors[labelCourant.getSommetCourant().getId()]= arc;
+                			
+                			//Verifie si la destination est atteinte - si oui on sort du for et du while
+                			if(data.getDestination()==labelCourant.getSommetCourant()) {
+                				destinationReached = true;
+                				break;
+                			}
+                		}
+            		}
+            	}
+        	}
+        	
         }
-        
         ShortestPathSolution solution;
 		// Destination has no predecessors, the solution is infeasible...
         if (arcSuccessors[data.getDestination().getId()] == null) {
--- be-graphes-gui/src/main/java/org/insa/graphs/gui/simple/Launch.java
+++ be-graphes-gui/src/main/java/org/insa/graphs/gui/simple/Launch.java
@@ -7,13 +7,18 @@ import java.awt.Dimension;
 import java.io.BufferedInputStream;
 import java.io.DataInputStream;
 import java.io.FileInputStream;
+import java.util.List;
 
 import javax.swing.JFrame;
 import javax.swing.SwingUtilities;
 
+import org.insa.graphs.algorithm.ArcInspector;
+import org.insa.graphs.algorithm.ArcInspectorFactory;
+import org.insa.graphs.algorithm.shortestpath.ShortestPathData;
 import org.insa.graphs.gui.drawing.Drawing;
 import org.insa.graphs.gui.drawing.components.BasicDrawing;
 import org.insa.graphs.model.Graph;
+import org.insa.graphs.model.Node;
 import org.insa.graphs.model.Path;
 import org.insa.graphs.model.io.BinaryGraphReader;
 import org.insa.graphs.model.io.GraphReader;
@@ -44,35 +49,50 @@ public class Launch {
         });
         return basicDrawing;
     }
-
+    
     public static void main(String[] args) throws Exception {
 
         // Visit these directory to see the list of available files on Commetud.
-        final String mapName = "/home/agbeti-m/Documents/BE Graphes/Maps/carre.mapgr";
+        final String mapName = "/home/agbeti-m/Documents/BE Graphes/Maps/insa.mapgr";
         final String pathName = "/mnt/commetud/3eme Annee MIC/Graphes-et-Algorithmes/Paths/path_fr31insa_rangueil_r2.path";
 
         // Create a graph reader.
         final GraphReader reader = new BinaryGraphReader(
                 new DataInputStream(new BufferedInputStream(new FileInputStream(mapName))));
-
+		//
         // TODO: Read the graph.
         final Graph graph = reader.read();
-
-        // Create the drawing:
-        final Drawing drawing = createDrawing();
-
-        // TODO: Draw the graph on the drawing.
-        drawing.drawGraph(graph);
-
-        // TODO: Create a PathReader.
-        final PathReader pathReader = new BinaryPathReader(
-                new DataInputStream(new BufferedInputStream(new FileInputStream(pathName))));
-
-        // TODO: Read the path.
-        final Path path = pathReader.readPath(graph);
-
-        // TODO: Draw the path.
-        drawing.drawPath(path);
+		//
+		//        // Create the drawing:
+		//        final Drawing drawing = createDrawing();
+		//
+		//        // TODO: Draw the graph on the drawing.
+		//        drawing.drawGraph(graph);
+		//
+		//        // TODO: Create a PathReader.
+		//        final PathReader pathReader = new BinaryPathReader(
+		//                new DataInputStream(new BufferedInputStream(new FileInputStream(pathName))));
+		//
+		//        // TODO: Read the path.
+		//        final Path path = pathReader.readPath(graph);
+		//
+		//        // TODO: Draw the path.
+		//        drawing.drawPath(path);
+        
+        //Test avec Bellman-Ford
+        int Origin_int =  0;
+        int Destination_int =  0;
+        int numInspector = 0;
+        
+        Node Origin = new Node(Origin_int, null);
+        Node Destination = new Node(Destination_int, null);
+        
+        List <ArcInspector> listInspector = new ArcInspectorFactory().getAllFilters();
+        ArcInspector arcInspector = listInspector.get(numInspector);
+        
+        ShortestPathData data = new ShortestPathData(graph, Origin, Destination, arcInspector);
+        //0 continuer ici: on vient de créer un data pour mettre en paramètre de bellman et dijkstra
+        
     }
 
 }
